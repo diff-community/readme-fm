2a61d18c4b206e539bc86318205aa7306e490c47
Shailen <shailen@gettruck.co.za>
2019-07-10 12:43:02 +0200

    Init

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy('public')
+
+  return {
+    dir: {
+      input: 'src/pages',
+      layouts: '../layouts',
+    },
+  };
+};
